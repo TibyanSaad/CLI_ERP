9267732e1d825e7b27842df3293c30607b1581eb
TibyanSaad <saad.tibyan02@gmail.com>
2026-04-01 12:31:09 +0400

    fully wroking snake w/proper file reading

--- snake/src/MoveSnake.java
+++ snake/src/MoveSnake.java
@@ -16,23 +16,29 @@ public class MoveSnake {
             // read file
             Path snakeFile = Path.of(MoveSnake.class.getResource("./map.txt").toURI());
             mapArray = (ArrayList<String>) Files.readAllLines(snakeFile);
-            mapArray2D = new char[mapArray.size()][mapArray.get(0).length()]; // create 2d array w/row and column size
+            for (int i = 0; i < mapArray.size(); i++) {
+                mapArray.set(i, mapArray.get(i).replace(" ", "")); // remove the spaces
+            }
+
+
+            mapArray2D = new char[mapArray.size()][mapArray.get(0).length()];// convert to 2d array w/row and column length
             for (int i = 0; i < mapArray.size(); i++) {
                 mapArray2D[i] = mapArray.get(i).toCharArray();// convert to char 2d array
             }
-        } catch (URISyntaxException | IOException e) {
+        } catch (IOException e) {
             System.err.println("ERROR READING FILE: " + e.getMessage()); // misreading the file
-            throw new RuntimeException(e); // exception handling of toURI and reading file
+            throw new RuntimeException(e);// exception handling of toURI and reading file
         }
 
         for (char[] row : mapArray2D) {
             for (char space : row) {
-                System.out.print(space + " ");
+                System.out.print(space+ " ");
             }
             System.out.println();
         }
 
-
+        System.out.println(mapArray2D.length);
+        System.out.println(mapArray2D[0].length);
         ArrayList<int[]> snakeBody = new ArrayList<>();
 
 
